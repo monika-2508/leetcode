d615022b8a3324e9fa882627241bfc441ff95a15
Monikasri <monikasribn@gmail.com>
2026-07-16 16:09:31 +0530

    Create 3790-FruitsIntoBasketsIi

---
+++ 3790-FruitsIntoBasketsIi/3790-FruitsIntoBasketsIi.java
@@ -0,0 +1,17 @@
+// Last updated: 7/16/2026, 4:09:24 PM
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int unplaced =0;
+        for(int i=0;i<fruits.length;i++){
+            boolean placed = false;
+            for(int j=0;j<baskets.length;j++){
+                 if(baskets[j]>=fruits[i] && baskets[j]!=-1){
+                    baskets[j] = -1;
+                    placed = true;
+                    break;
+                 }
+            }if(!placed) unplaced++;
+        }
+        return unplaced;
+    }
+}
