c5efbd4475a665b3ce44692e60bd4e5fefde6f08
Monikasri <monikasribn@gmail.com>
2026-07-16 16:12:58 +0530

    Create 182-DuplicateEmails

---
+++ 182-DuplicateEmails/182-DuplicateEmails.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/16/2026, 4:12:51 PM
+# Write your MySQL query statement below
+SELECT 
+    email AS Email
+FROM 
+    Person
+GROUP BY 
+    email
+HAVING 
+    COUNT(email) > 1;
