9ba12b4f9ec48f46f781f4c6ba72e37d3ebf7d31
Monikasri <monikasribn@gmail.com>
2026-07-16 16:12:14 +0530

    Create 394-DecodeString

---
+++ 394-DecodeString/394-DecodeString.java
@@ -0,0 +1,31 @@
+// Last updated: 7/16/2026, 4:12:07 PM
+class Solution {
+    int i = 0;
+    public String decodeString(String s) {
+        i = 0;
+        return decode(s);
+    }
+    private String decode(String s) {
+        StringBuilder res = new StringBuilder();
+        int num = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+                i++;
+            } else if (c == '[') {
+                i++;
+                String inner = decode(s);
+                for (int k = 0; k < num; k++) res.append(inner);
+                num = 0;
+            } else if (c == ']') {
+                i++;
+                return res.toString();
+            } else {
+                res.append(c);
+                i++;
+            }
+        }
+        return res.toString();
+    }
+}
