443f772ffd83ed75ea2558582613774841bb9cb4
Monikasri <monikasribn@gmail.com>
2026-07-16 16:11:25 +0530

    Create 783-SearchInABinarySearchTree

---
+++ 783-SearchInABinarySearchTree/783-SearchInABinarySearchTree.java
@@ -0,0 +1,43 @@
+// Last updated: 7/16/2026, 4:11:18 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        // while(root!=null){
+        //     if(root.val==val){
+        //         return root;
+        //     }
+        //     else if(val<root.val){
+        //         root=root.left;
+        //     }
+        //     else{
+        //         root=root.right;
+        //     }
+        // }
+        // return null;
+
+
+        //or
+        if(root==null || root.val==val){
+            return root;
+        }
+
+        if(val < root.val){
+            return searchBST(root.left,val);
+        }
+        return searchBST(root.right,val);
+    }
+}
