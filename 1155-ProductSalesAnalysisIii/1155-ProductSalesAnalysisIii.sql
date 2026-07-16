ed0bca3ca90ed0c4abdcfed8a34b669a61706d25
Monikasri <monikasribn@gmail.com>
2026-07-16 16:10:58 +0530

    Create 1155-ProductSalesAnalysisIii

---
+++ 1155-ProductSalesAnalysisIii/1155-ProductSalesAnalysisIii.sql
@@ -0,0 +1,19 @@
+-- Last updated: 7/16/2026, 4:10:50 PM
+# Write your MySQL query statement below
+SELECT 
+    product_id, 
+    year AS first_year, 
+    quantity, 
+    price
+FROM 
+    Sales
+WHERE 
+    (product_id, year) IN (
+        SELECT 
+            product_id, 
+            MIN(year) 
+        FROM 
+            Sales 
+        GROUP BY 
+            product_id
+    );
