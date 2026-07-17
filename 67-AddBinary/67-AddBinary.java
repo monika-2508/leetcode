5bcff2fd6fcbe5c36127af6ffb7dd64035a170ac
Monikasri <monikasribn@gmail.com>
2026-07-17 14:51:52 +0530

    File updated at 7/17/2026, 2:51:44 PM

--- 67-AddBinary/67-AddBinary.java
+++ 67-AddBinary/67-AddBinary.java
@@ -1,41 +1,37 @@
-// Last updated: 7/17/2026, 2:50:56 PM
+// Last updated: 7/17/2026, 2:51:44 PM
 1class Solution {
-2    public int longestValidParentheses(String s) {
-3        int maxLen = 0;
-4        int left = 0;
-5        int right = 0;
-6
-7        for (int i = 0; i < s.length(); i++) {
-8            if (s.charAt(i) == '(') {
-9                left++;
-10            } else {
-11                right++;
-12            }
-13            if (left == right) {
-14                maxLen = Math.max(maxLen, 2 * right);
-15            } else if (right > left) {
-16                left = 0;
-17                right = 0;
-18            }
-19        }
-20
-21        left = 0;
-22        right = 0;
-23
-24        for (int i = s.length() - 1; i >= 0; i--) {
-25            if (s.charAt(i) == '(') {
-26                left++;
-27            } else {
-28                right++;
-29            }
-30            if (left == right) {
-31                maxLen = Math.max(maxLen, 2 * left);
-32            } else if (left > right) {
-33                left = 0;
-34                right = 0;
-35            }
-36        }
-37
-38        return maxLen;
-39    }
-40}
+2    public boolean isNumber(String s) {
+3        s = s.trim();
+4        
+5        boolean seenDigit = false;
+6        boolean seenDot = false;
+7        boolean seenE = false;
+8        
+9        for (int i = 0; i < s.length(); i++) {
+10            char ch = s.charAt(i);
+11            
+12            if (Character.isDigit(ch)) {
+13                seenDigit = true;
+14            } else if (ch == '+' || ch == '-') {
+15                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+16                    return false;
+17                }
+18            } else if (ch == 'e' || ch == 'E') {
+19                if (seenE || !seenDigit) {
+20                    return false;
+21                }
+22                seenE = true;
+23                seenDigit = false; 
+24            } else if (ch == '.') {
+25                if (seenDot || seenE) {
+26                    return false;
+27                }
+28                seenDot = true;
+29            } else {
+30                return false;
+31            }
+32        }
+33        
+34        return seenDigit;
+35    }
+36}
