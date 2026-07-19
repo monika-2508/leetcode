671b2bb3ebe407f2a8934d7cb3c8dc188dd34229
Monikasri <monikasribn@gmail.com>
2026-07-19 16:35:32 +0530

    File updated at 7/19/2026, 4:35:24 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,12 +1,29 @@
-// Last updated: 7/16/2026, 4:12:28 PM
-class Solution {
-    public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int expected = n*(n+1)/2;
-        int actual = 0;
-        for(int x:nums){
-            actual += x;
-        }
-        return expected - actual;
-    }
-}
+// Last updated: 7/19/2026, 4:35:24 PM
+1class Solution {
+2    public int missingNumber(int[] nums) {
+3        int i=0;
+4        while(i<nums.length){
+5          
+6            int correct = nums[i];
+7            if(nums[i]<nums.length && nums[i] != nums[correct]){
+8                swap(nums,i,correct);
+9            }
+10            else{ 
+11                i++;
+12            }
+13
+14        }
+15        for(int j=0;j<nums.length;j++){
+16            if(nums[j]!=j){
+17                return j;
+18            }
+19        }
+20        return nums.length;
+21
+22    }
+23    public static void swap(int[] arr,int first, int second){
+24        int temp = arr[first];
+25        arr[first] = arr[second];
+26        arr[second] = temp;
+27    }
+28}
