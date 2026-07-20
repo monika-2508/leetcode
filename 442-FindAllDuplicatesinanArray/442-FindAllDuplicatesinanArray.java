56b0fbd9110f2646a8415157d8bdd587a04ffa02
Monikasri <monikasribn@gmail.com>
2026-07-21 00:22:24 +0530

    File updated at 7/21/2026, 12:22:15 AM

--- 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.java
+++ 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.java
@@ -1,27 +1,29 @@
-// Last updated: 7/21/2026, 12:01:43 AM
+// Last updated: 7/21/2026, 12:22:15 AM
 1class Solution {
-2    public List<Integer> findDuplicates(int[] nums) {
-3        int i = 0;
+2    public int[] findErrorNums(int[] nums) {
+3        int i=0;
 4        while(i<nums.length){
-5            int correct = nums[i] - 1;
-6            if(nums[i] != nums[correct]){
-7                swap(nums,i,correct);
-8            }
-9            else{
-10                i++;
-11            }
-12        }
-13        List<Integer> ans = new ArrayList<>();
-14        for(int index=0;index<nums.length;index++){
-15            if(nums[index] != index+1){
-16                ans.add(nums[index]);
-17            }
-18        }
-19        return ans;
-20    }
-21    public static void swap(int[] nums,int i, int correct){
-22        int temp = nums[i];
-23        nums[i] = nums[correct];
-24        nums[correct]= temp;
-25    }
-26}
+5            
+6            int correct = nums[i] - 1;
+7            if(nums[i] != nums[correct]){
+8                swap(nums,i,correct);
+9            }
+10            else{
+11                i++;
+12            }
+13            
+14        }
+15        for(int index = 0;index < nums.length;index++){
+16            if(nums[index] != index+1){
+17                return new int[] {nums[index], index+1};
+18            }
+19        }
+20
+21        return new int[] {-1, -1};
+22    }
+23    public static void swap(int[] nums, int first, int second){
+24        int temp = nums[first];
+25        nums[first] = nums[second];
+26        nums[second] = temp;
+27    }
+28}
