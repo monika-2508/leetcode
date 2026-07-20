43569b3f88a9f5ff405c5c9cf4e43915363177ca
Monikasri <monikasribn@gmail.com>
2026-07-20 23:51:15 +0530

    File updated at 7/20/2026, 11:51:07 PM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -1,28 +1,30 @@
-// Last updated: 7/20/2026, 11:47:57 PM
+// Last updated: 7/20/2026, 11:51:07 PM
 1class Solution {
 2    public int findDuplicate(int[] nums) {
 3       int i=0;
-4        while(i<nums.length){
-5            int correct = nums[i]-1;
-6            if(nums[i] != nums[correct]){
-7                swap(nums,i,correct);
-8
-9            }
-10            else{
-11                i++;
-12            }
-13        }
-14        for(int index=0;index<nums.length;index++){
-15            if(nums[index] != index+1){
-16                return nums[index];
-17            }
-18        }
-19        return -1;
-20    }
-21    void swap(int[] a,int first, int second){
-22        int temp = a[first];
-23        a[first] = a[second];
-24        a[second] = temp;
-25
-26    }
-27}
+4     
+5        while(i<nums.length){
+6            if(nums[i] != i+1)
+7            {
+8                int correct = nums[i]-1;
+9                if(nums[i] != nums[correct]){
+10                    swap(nums,i,correct);
+11
+12                }
+13                else{
+14                    return nums[i];
+15                }
+16            }
+17            else{
+18                i++;
+19            }
+20        }
+21        return -1;
+22    }
+23    void swap(int[] a,int first, int second){
+24        int temp = a[first];
+25        a[first] = a[second];
+26        a[second] = temp;
+27
+28    }
+29}
