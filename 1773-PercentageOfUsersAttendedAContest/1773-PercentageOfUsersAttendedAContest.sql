a8015ca955b8a8a144ffe60e14c4653eb1801074
Monikasri <monikasribn@gmail.com>
2026-07-25 09:16:11 +0530

    Create 1773-PercentageOfUsersAttendedAContest

---
+++ 1773-PercentageOfUsersAttendedAContest/1773-PercentageOfUsersAttendedAContest.sql
@@ -0,0 +1,12 @@
+-- Last updated: 7/25/2026, 9:16:02 AM
+# Write your MySQL query statement below
+SELECT 
+    contest_id,
+    ROUND(COUNT(DISTINCT user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM 
+    Register
+GROUP BY 
+    contest_id
+ORDER BY 
+    percentage DESC,
+    contest_id ASC;
