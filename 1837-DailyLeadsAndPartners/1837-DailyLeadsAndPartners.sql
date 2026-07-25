df5be991268b1b2826fcf2f4fd6ded22dab0647f
Monikasri <monikasribn@gmail.com>
2026-07-25 09:16:06 +0530

    Create 1837-DailyLeadsAndPartners

---
+++ 1837-DailyLeadsAndPartners/1837-DailyLeadsAndPartners.sql
@@ -0,0 +1,12 @@
+-- Last updated: 7/25/2026, 9:15:57 AM
+# Write your MySQL query statement below
+SELECT 
+    date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) AS unique_leads,
+    COUNT(DISTINCT partner_id) AS unique_partners
+FROM 
+    DailySales
+GROUP BY 
+    date_id, 
+    make_name;
