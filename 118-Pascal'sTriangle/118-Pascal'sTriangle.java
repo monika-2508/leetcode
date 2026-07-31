e6f14cc208dac9889b9073f20e4a5d07b51ec882
Monikasri <monikasribn@gmail.com>
2026-07-31 09:22:00 +0530

    Create 118-Pascal'sTriangle

---
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.java
@@ -0,0 +1,15 @@
+// Last updated: 7/31/2026, 9:21:49 AM
+1class Solution {
+2    public String convertToTitle(int columnNumber) {
+3        StringBuilder result = new StringBuilder();
+4
+5        while (columnNumber > 0) {
+6            columnNumber--; // Adjust for 1-based indexing (0 -> 'A', 25 -> 'Z')
+7            char remainder = (char) ('A' + (columnNumber % 26));
+8            result.append(remainder);
+9            columnNumber /= 26;
+10        }
+11
+12        return result.reverse().toString();
+13    }
+14}
