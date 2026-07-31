ed22e2cef7e23cbdcdf25c003298850192666f8c
Monikasri <monikasribn@gmail.com>
2026-07-31 09:06:58 +0530

    File updated at 7/31/2026, 9:06:48 AM

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,44 +1,66 @@
-// Last updated: 7/31/2026, 9:05:23 AM
-1class Solution {
-2    public boolean isMatch(String s, String p) {
-3        int m = s.length();
-4        int n = p.length();
-5
-6        // dp[i][j] represents if s[0...i-1] matches p[0...j-1]
-7        boolean[][] dp = new boolean[m + 1][n + 1];
-8
-9        // Base case: empty string matches empty pattern
-10        dp[0][0] = true;
-11
-12        // Base case: patterns like "a*", "a*b*", "a*b*c*" can match an empty string
-13        for (int j = 2; j <= n; j++) {
-14            if (p.charAt(j - 1) == '*') {
-15                dp[0][j] = dp[0][j - 2];
-16            }
-17        }
-18
-19        // Fill DP table
-20        for (int i = 1; i <= m; i++) {
-21            for (int j = 1; j <= n; j++) {
-22                char charS = s.charAt(i - 1);
-23                char charP = p.charAt(j - 1);
-24
-25                if (charP == '.' || charP == charS) {
-26                    // Match current single character
-27                    dp[i][j] = dp[i - 1][j - 1];
-28                } else if (charP == '*') {
-29                    // Option 1: Treat '*' as 0 occurrences of preceding character
-30                    dp[i][j] = dp[i][j - 2];
-31
-32                    // Option 2: Treat '*' as 1 or more occurrences (if preceding character matches)
-33                    char prevP = p.charAt(j - 2);
-34                    if (prevP == '.' || prevP == charS) {
-35                        dp[i][j] = dp[i][j] || dp[i - 1][j];
-36                    }
-37                }
-38            }
-39        }
-40
-41        return dp[m][n];
-42    }
-43}
+// Last updated: 7/31/2026, 9:06:48 AM
+1import java.util.ArrayList;
+2import java.util.List;
+3
+4class Solution {
+5    public List<String> fullJustify(String[] words, int maxWidth) {
+6        List<String> result = new ArrayList<>();
+7        int i = 0;
+8        int n = words.length;
+9
+10        while (i < n) {
+11            int j = i + 1;
+12            int lineLength = words[i].length();
+13
+14            // Determine how many words can fit into the current line
+15            while (j < n && lineLength + 1 + words[j].length() <= maxWidth) {
+16                lineLength += 1 + words[j].length();
+17                j++;
+18            }
+19
+20            int numOfWords = j - i;
+21            StringBuilder sb = new StringBuilder();
+22
+23            // Case 1: Last line or a line with only one word -> Left-justified
+24            if (j == n || numOfWords == 1) {
+25                for (int k = i; k < j; k++) {
+26                    sb.append(words[k]);
+27                    if (k < j - 1) {
+28                        sb.append(" ");
+29                    }
+30                }
+31                // Pad remaining spaces to the right
+32                while (sb.length() < maxWidth) {
+33                    sb.append(" ");
+34                }
+35            } 
+36            // Case 2: Middle line with multiple words -> Fully (left and right) justified
+37            else {
+38                int totalWordChars = 0;
+39                for (int k = i; k < j; k++) {
+40                    totalWordChars += words[k].length();
+41                }
+42
+43                int totalSpaces = maxWidth - totalWordChars;
+44                int spacesBetweenSlots = totalSpaces / (numOfWords - 1);
+45                int extraSpaces = totalSpaces % (numOfWords - 1);
+46
+47                for (int k = i; k < j; k++) {
+48                    sb.append(words[k]);
+49                    if (k < j - 1) {
+50                        // Base spaces for each slot
+51                        int spacesToApply = spacesBetweenSlots + (k - i < extraSpaces ? 1 : 0);
+52                        for (int s = 0; s < spacesToApply; s++) {
+53                            sb.append(" ");
+54                        }
+55                    }
+56                }
+57            }
+58
+59            result.add(sb.toString());
+60            i = j; // Move to the next line's starting word
+61        }
+62
+63        return result;
+64    }
+65}
