ccee5e43eccdba40a2461bc6d17babd053e86954
Monikasri <monikasribn@gmail.com>
2026-07-31 09:42:50 +0530

    File updated at 7/31/2026, 9:42:40 AM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.java
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -1,22 +1,25 @@
-// Last updated: 7/16/2026, 4:13:55 PM
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-
-        String prefix = strs[0];
-
-        for (int i = 1; i < strs.length; i++) {
-            while (!strs[i].startsWith(prefix)) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-
-                if (prefix.isEmpty()) {
-                    return "";
-                }
-            }
-        }
-
-        return prefix;
-    }
-}
+// Last updated: 7/31/2026, 9:42:40 AM
+1class Solution {
+2    public String longestCommonPrefix(String[] strs) {
+3        if (strs == null || strs.length == 0) {
+4            return "";
+5        }
+6
+7        // Start by assuming the first string is the common prefix
+8        String prefix = strs[0];
+9
+10        // Compare the prefix with each string in the array
+11        for (int i = 1; i < strs.length; i++) {
+12            // Trim the prefix until strs[i] starts with it
+13            while (strs[i].indexOf(prefix) != 0) {
+14                prefix = prefix.substring(0, prefix.length() - 1);
+15                
+16                if (prefix.isEmpty()) {
+17                    return "";
+18                }
+19            }
+20        }
+21
+22        return prefix;
+23    }
+24}
