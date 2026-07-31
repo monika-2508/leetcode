d3bbf26e97567e9d861d531583d980fbd0503cef
Monikasri <monikasribn@gmail.com>
2026-07-31 09:25:07 +0530

    File updated at 7/31/2026, 9:24:57 AM

--- 118-Pascal'sTriangle/118-Pascal'sTriangle.java
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.java
@@ -1,17 +1,14 @@
-// Last updated: 7/31/2026, 9:23:25 AM
+// Last updated: 7/31/2026, 9:24:57 AM
 1class Solution {
-2    public int majorityElement(int[] nums) {
-3        int candidate = 0;
-4        int count = 0;
-5
-6        for (int num : nums) {
-7            if (count == 0) {
-8                candidate = num;
-9            }
-10            
-11            count += (num == candidate) ? 1 : -1;
-12        }
-13
-14        return candidate;
-15    }
-16}
+2    public int titleToNumber(String columnTitle) {
+3        int result = 0;
+4
+5        for (int i = 0; i < columnTitle.length(); i++) {
+6            char c = columnTitle.charAt(i);
+7            int value = c - 'A' + 1; // 'A' -> 1, 'B' -> 2, ..., 'Z' -> 26
+8            result = result * 26 + value;
+9        }
+10
+11        return result;
+12    }
+13}
