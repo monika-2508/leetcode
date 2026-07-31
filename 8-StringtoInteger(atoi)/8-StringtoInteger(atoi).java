a441cf6c025f923746e1ba0fa9786502cbe63eb0
Monikasri <monikasribn@gmail.com>
2026-07-31 09:18:14 +0530

    File updated at 7/31/2026, 9:18:04 AM

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,47 +1,29 @@
-// Last updated: 7/31/2026, 9:16:38 AM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public boolean isBalanced(TreeNode root) {
-18        return checkHeight(root) != -1;
-19    }
-20
-21    private int checkHeight(TreeNode node) {
-22        if (node == null) {
-23            return 0;
+// Last updated: 7/31/2026, 9:18:04 AM
+1import java.util.ArrayList;
+2import java.util.List;
+3
+4class Solution {
+5    public List<List<Integer>> generate(int numRows) {
+6        List<List<Integer>> triangle = new ArrayList<>();
+7
+8        for (int i = 0; i < numRows; i++) {
+9            List<Integer> row = new ArrayList<>();
+10            
+11            for (int j = 0; j <= i; j++) {
+12                // The first and last elements of each row are always 1
+13                if (j == 0 || j == i) {
+14                    row.add(1);
+15                } else {
+16                    // Middle elements: sum of the two elements directly above in the previous row
+17                    int prevRowVal1 = triangle.get(i - 1).get(j - 1);
+18                    int prevRowVal2 = triangle.get(i - 1).get(j);
+19                    row.add(prevRowVal1 + prevRowVal2);
+20                }
+21            }
+22            
+23            triangle.add(row);
 24        }
 25
-26        // Check left subtree
-27        int leftHeight = checkHeight(node.left);
-28        if (leftHeight == -1) {
-29            return -1; // Left subtree is unbalanced
-30        }
-31
-32        // Check right subtree
-33        int rightHeight = checkHeight(node.right);
-34        if (rightHeight == -1) {
-35            return -1; // Right subtree is unbalanced
-36        }
-37
-38        // If height difference between left and right subtrees > 1, unbalanced
-39        if (Math.abs(leftHeight - rightHeight) > 1) {
-40            return -1;
-41        }
-42
-43        // Return actual height of current node
-44        return Math.max(leftHeight, rightHeight) + 1;
-45    }
-46}
+26        return triangle;
+27    }
+28}
