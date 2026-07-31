2348f6f37c44bad218ce89f5eabf269877b09cf4
Monikasri <monikasribn@gmail.com>
2026-07-31 09:11:31 +0530

    File updated at 7/31/2026, 9:11:20 AM

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,61 +1,32 @@
-// Last updated: 7/31/2026, 9:07:57 AM
-1import java.util.HashMap;
-2import java.util.Map;
-3
-4class Solution {
-5    private Map<String, Boolean> memo = new HashMap<>();
-6
-7    public boolean isScramble(String s1, String s2) {
-8        // Base case 1: Strings are identical
-9        if (s1.equals(s2)) {
-10            return true;
-11        }
-12
-13        // Base case 2: String lengths differ (should not happen per constraints)
-14        if (s1.length() != s2.length()) {
-15            return false;
-16        }
-17
-18        // Check memoization table
-19        String key = s1 + "_" + s2;
-20        if (memo.containsKey(key)) {
-21            return memo.get(key);
-22        }
-23
-24        // Pruning: If character counts do not match, s2 cannot be a scrambled version of s1
-25        int[] count = new int[26];
-26        int len = s1.length();
-27        for (int i = 0; i < len; i++) {
-28            count[s1.charAt(i) - 'a']++;
-29            count[s2.charAt(i) - 'a']--;
-30        }
-31        for (int c : count) {
-32            if (c != 0) {
-33                memo.put(key, false);
-34                return false;
-35            }
-36        }
-37
-38        // Try every possible split position i
-39        for (int i = 1; i < len; i++) {
-40            // Case 1: Substrings are NOT swapped
-41            // s1[0...i-1] matches s2[0...i-1] AND s1[i...len-1] matches s2[i...len-1]
-42            if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
-43                isScramble(s1.substring(i), s2.substring(i))) {
-44                memo.put(key, true);
-45                return true;
-46            }
-47
-48            // Case 2: Substrings ARE swapped
-49            // s1[0...i-1] matches s2[len-i...len-1] AND s1[i...len-1] matches s2[0...len-i-1]
-50            if (isScramble(s1.substring(0, i), s2.substring(len - i)) &&
-51                isScramble(s1.substring(i), s2.substring(0, len - i))) {
-52                memo.put(key, true);
-53                return true;
-54            }
-55        }
-56
-57        memo.put(key, false);
-58        return false;
-59    }
-60}
+// Last updated: 7/31/2026, 9:11:20 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isSameTree(TreeNode p, TreeNode q) {
+18        // Base case: Both nodes are null -> identical
+19        if (p == null && q == null) {
+20            return true;
+21        }
+22
+23        // Base case: One node is null or values differ -> not identical
+24        if (p == null || q == null || p.val != q.val) {
+25            return false;
+26        }
+27
+28        // Recursively compare left and right subtrees
+29        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+30    }
+31}
