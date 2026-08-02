5df6a3440bc1f881b565210a8ca37a7ef3839e48
Monikasri <monikasribn@gmail.com>
2026-08-02 12:45:17 +0530

    File updated at 8/2/2026, 12:45:06 PM

--- 41-FirstMissingPositive/41-FirstMissingPositive.java
+++ 41-FirstMissingPositive/41-FirstMissingPositive.java
@@ -1,20 +1,27 @@
-// Last updated: 7/16/2026, 4:13:43 PM
-class Solution {
-    public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        for(int i=0;i<n;i++){
-            while(nums[i]>=1 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
-                int cor=nums[i]-1;
-                int temp=nums[i];
-                nums[i]=nums[cor];
-                nums[cor]=temp;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i]!=i+1){
-                return i+1;
-            }
-        }
-        return n+1;
-    }
-}
+// Last updated: 8/2/2026, 12:45:06 PM
+1class Solution {
+2    public int firstMissingPositive(int[] nums) {
+3        int i=0;
+4        while(i<nums.length){
+5            int correct = nums[i] - 1;
+6            if(nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[correct]){
+7                swap(nums, i, correct);
+8            }
+9            else{
+10                i++;
+11            }
+12        }
+13        for(int index = 0; index < nums.length; index++){
+14            if(nums[index] != index + 1){
+15                return index + 1;
+16            }
+17        }
+18        return nums.length + 1;
+19    }
+20    static void swap(int[] nums, int first, int second){
+21        int temp = nums[first];
+22        nums[first] = nums[second];
+23        nums[second] = temp;
+24        
+25    }
+26}
