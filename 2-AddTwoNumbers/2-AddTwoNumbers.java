b3bf8cdec0a38a40cebaca14f873834c7641d0f6
Monikasri <monikasribn@gmail.com>
2026-08-11 08:56:47 +0530

    File updated at 8/11/2026, 8:56:35 AM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,32 +1,34 @@
-// Last updated: 8/11/2026, 8:54:12 AM
+// Last updated: 8/11/2026, 8:56:35 AM
 1class Solution {
-2    public String longestPalindrome(String s) {
-3        if (s == null || s.length() < 1) return "";
-4        
-5        int start = 0, end = 0;
-6        
-7        for (int i = 0; i < s.length(); i++) {
-8            // Expand around single character center (odd length palindromes)
-9            int len1 = expandAroundCenter(s, i, i);
-10            // Expand around two-character center (even length palindromes)
-11            int len2 = expandAroundCenter(s, i, i + 1);
-12            
-13            int maxLen = Math.max(len1, len2);
-14            
-15            if (maxLen > end - start) {
-16                start = i - (maxLen - 1) / 2;
-17                end = i + maxLen / 2;
-18            }
-19        }
-20        
-21        return s.substring(start, end + 1);
-22    }
-23    
-24    private int expandAroundCenter(String s, int left, int right) {
-25        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-26            left--;
-27            right++;
-28        }
-29        return right - left - 1;
-30    }
-31}
+2    public String convert(String s, int numRows) {
+3        if (numRows == 1 || s.length() <= numRows) {
+4            return s;
+5        }
+6
+7        StringBuilder[] rows = new StringBuilder[numRows];
+8        for (int i = 0; i < numRows; i++) {
+9            rows[i] = new StringBuilder();
+10        }
+11
+12        int currentRow = 0;
+13        boolean goingDown = false;
+14
+15        for (char c : s.toCharArray()) {
+16            rows[currentRow].append(c);
+17            
+18            // Change direction when reaching top or bottom row
+19            if (currentRow == 0 || currentRow == numRows - 1) {
+20                goingDown = !goingDown;
+21            }
+22
+23            currentRow += goingDown ? 1 : -1;
+24        }
+25
+26        StringBuilder result = new StringBuilder();
+27        for (StringBuilder row : rows) {
+28            result.append(row);
+29        }
+30
+31        return result.toString();
+32    }
+33}
