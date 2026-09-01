dc3a0e15c951bda6b2c7768e8d050631d66ac506
Monikasri <monikasribn@gmail.com>
2026-09-01 09:29:33 +0530

    Create 5-LongestPalindromicSubstring

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -0,0 +1,32 @@
+// Last updated: 9/1/2026, 9:29:17 AM
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            // Expand around single character center (odd length palindromes)
+            int len1 = expandAroundCenter(s, i, i);
+            // Expand around two-character center (even length palindromes)
+            int len2 = expandAroundCenter(s, i, i + 1);
+            
+            int maxLen = Math.max(len1, len2);
+            
+            if (maxLen > end - start) {
+                start = i - (maxLen - 1) / 2;
+                end = i + maxLen / 2;
+            }
+        }
+        
+        return s.substring(start, end + 1);
+    }
+    
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
