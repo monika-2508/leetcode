ada15179f7b3e4db6ef09fd5ede407f2b0759b26
Monikasri <monikasribn@gmail.com>
2026-09-01 09:00:53 +0530

    File updated at 9/1/2026, 9:00:38 AM

--- 112-PathSum/112-PathSum.java
+++ 112-PathSum/112-PathSum.java
@@ -1,21 +1,19 @@
-// Last updated: 9/1/2026, 8:59:27 AM
+// Last updated: 9/1/2026, 9:00:38 AM
 1import java.util.ArrayList;
 2import java.util.List;
 3
 4class Solution {
-5    public List<Integer> getRow(int rowIndex) {
-6        List<Integer> row = new ArrayList<>();
-7        row.add(1);
-8
-9        for (int i = 1; i <= rowIndex; i++) {
-10            // Update the existing values from right to left
-11            for (int j = i - 1; j > 0; j--) {
-12                row.set(j, row.get(j) + row.get(j - 1));
-13            }
-14            // Each row ends with 1
-15            row.add(1);
-16        }
-17
-18        return row;
-19    }
-20}
+5    public List<Integer> postorderTraversal(TreeNode root) {
+6        List<Integer> result = new ArrayList<>();
+7        dfs(root, result);
+8        return result;
+9    }
+10
+11    private void dfs(TreeNode node, List<Integer> result) {
+12        if (node == null) return;
+13
+14        dfs(node.left, result);    // Left
+15        dfs(node.right, result);   // Right
+16        result.add(node.val);      // Root
+17    }
+18}
