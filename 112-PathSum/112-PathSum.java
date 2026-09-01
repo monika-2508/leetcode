f9afde5bd69497c5685410c09ec7153d2e970100
Monikasri <monikasribn@gmail.com>
2026-09-01 08:59:43 +0530

    File updated at 9/1/2026, 8:59:27 AM

--- 112-PathSum/112-PathSum.java
+++ 112-PathSum/112-PathSum.java
@@ -1,28 +1,21 @@
-// Last updated: 7/16/2026, 4:13:18 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null){
-            return false;
-        }
-        if(root.left == null && root.right == null){
-            return root.val == targetSum;
-        }
-        int remainingSum = targetSum - root.val;
-        return hasPathSum(root.left, remainingSum) || hasPathSum(root.right, remainingSum);
-    }
-}
+// Last updated: 9/1/2026, 8:59:27 AM
+1import java.util.ArrayList;
+2import java.util.List;
+3
+4class Solution {
+5    public List<Integer> getRow(int rowIndex) {
+6        List<Integer> row = new ArrayList<>();
+7        row.add(1);
+8
+9        for (int i = 1; i <= rowIndex; i++) {
+10            // Update the existing values from right to left
+11            for (int j = i - 1; j > 0; j--) {
+12                row.set(j, row.get(j) + row.get(j - 1));
+13            }
+14            // Each row ends with 1
+15            row.add(1);
+16        }
+17
+18        return row;
+19    }
+20}
