bfb876b48dd2240e9b0e4908bb4dd61f9a2b0149
Monikasri <monikasribn@gmail.com>
2026-09-01 09:23:36 +0530

    File updated at 9/1/2026, 9:23:20 AM

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,26 +1,17 @@
-// Last updated: 9/1/2026, 9:22:10 AM
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode reverseList(ListNode head) {
-13        ListNode prev = null;
-14        ListNode curr = head;
-15
-16        while (curr != null) {
-17            ListNode nextTemp = curr.next; // Store next node
-18            curr.next = prev;              // Reverse current pointer
-19            prev = curr;                   // Move prev forward
-20            curr = nextTemp;               // Move curr forward
-21        }
-22
-23        return prev; // New head of reversed list
-24    }
-25}
+// Last updated: 9/1/2026, 9:23:20 AM
+1class Solution {
+2    public int strStr(String haystack, String needle) {
+3        int hLen = haystack.length();
+4        int nLen = needle.length();
+5
+6        if (nLen > hLen) return -1;
+7
+8        for (int i = 0; i <= hLen - nLen; i++) {
+9            if (haystack.substring(i, i + nLen).equals(needle)) {
+10                return i;
+11            }
+12        }
+13
+14        return -1;
+15    }
+16}
