3414a2e13b0a54296ebf426c62ca734ae5456f85
Monikasri <monikasribn@gmail.com>
2026-09-01 09:25:08 +0530

    File updated at 9/1/2026, 9:24:52 AM

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,17 +1,28 @@
-// Last updated: 9/1/2026, 9:23:20 AM
-1class Solution {
-2    public int strStr(String haystack, String needle) {
-3        int hLen = haystack.length();
-4        int nLen = needle.length();
-5
-6        if (nLen > hLen) return -1;
-7
-8        for (int i = 0; i <= hLen - nLen; i++) {
-9            if (haystack.substring(i, i + nLen).equals(needle)) {
-10                return i;
-11            }
-12        }
-13
-14        return -1;
-15    }
-16}
+// Last updated: 9/1/2026, 9:24:52 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode deleteDuplicates(ListNode head) {
+13        ListNode curr = head;
+14
+15        while (curr != null && curr.next != null) {
+16            if (curr.val == curr.next.val) {
+17                // Skip the duplicate node
+18                curr.next = curr.next.next;
+19            } else {
+20                // Move to next distinct node
+21                curr = curr.next;
+22            }
+23        }
+24
+25        return head;
+26    }
+27}
