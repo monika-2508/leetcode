d7e1485ec1e2a9fe500175c6762533fd5f72576e
Monikasri <monikasribn@gmail.com>
2026-09-01 09:19:48 +0530

    Create 2057-CountSalaryCategories

---
+++ 2057-CountSalaryCategories/2057-CountSalaryCategories.sql
@@ -0,0 +1,23 @@
+-- Last updated: 9/1/2026, 9:19:32 AM
+# Write your MySQL query statement below
+SELECT 
+    'Low Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income < 20000
+
+UNION ALL
+
+SELECT 
+    'Average Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income >= 20000 AND income <= 50000
+
+UNION ALL
+
+SELECT 
+    'High Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income > 50000;
