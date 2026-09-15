0ec3a14af7d7c3870180befa90f3adb0a74bef3a
Monikasri <monikasribn@gmail.com>
2026-09-15 09:24:56 +0530

    File updated at 9/15/2026, 9:24:37 AM

--- 37-SudokuSolver/37-SudokuSolver.java
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -1,44 +1,34 @@
-// Last updated: 9/15/2026, 9:20:43 AM
-1class Solution {
-2    private final String[] LESS_THAN_20 = {
-3        "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-4        "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
-5    };
-6
-7    private final String[] TENS = {
-8        "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
-9    };
-10
-11    private final String[] THOUSANDS = {
-12        "", "Thousand", "Million", "Billion"
-13    };
-14
-15    public String numberToWords(int num) {
-16        if (num == 0) return "Zero";
-17
-18        int i = 0;
-19        String words = "";
-20
-21        while (num > 0) {
-22            if (num % 1000 != 0) {
-23                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
-24            }
-25            num /= 1000;
-26            i++;
-27        }
-28
-29        return words.trim();
-30    }
-31
-32    private String helper(int num) {
-33        if (num == 0) {
-34            return "";
-35        } else if (num < 20) {
-36            return LESS_THAN_20[num] + " ";
-37        } else if (num < 100) {
-38            return TENS[num / 10] + " " + helper(num % 10);
-39        } else {
-40            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
-41        }
-42    }
-43}
+// Last updated: 9/15/2026, 9:24:37 AM
+1import java.util.Collections;
+2import java.util.PriorityQueue;
+3
+4class MedianFinder {
+5    // Max-heap stores the smaller half of numbers
+6    private PriorityQueue<Integer> maxHeap;
+7    // Min-heap stores the larger half of numbers
+8    private PriorityQueue<Integer> minHeap;
+9
+10    public MedianFinder() {
+11        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+12        minHeap = new PriorityQueue<>();
+13    }
+14    
+15    public void addNum(int num) {
+16        maxHeap.offer(num);
+17        // Ensure all elements in maxHeap <= elements in minHeap
+18        minHeap.offer(maxHeap.poll());
+19        
+20        // Balance heap sizes so maxHeap has equal or 1 more element than minHeap
+21        if (maxHeap.size() < minHeap.size()) {
+22            maxHeap.offer(minHeap.poll());
+23        }
+24    }
+25    
+26    public double findMedian() {
+27        if (maxHeap.size() > minHeap.size()) {
+28            return maxHeap.peek();
+29        } else {
+30            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+31        }
+32    }
+33}
