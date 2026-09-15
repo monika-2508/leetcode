d7aa8a7624effd780d7b39b9bb887f14effc81a8
Monikasri <monikasribn@gmail.com>
2026-09-15 09:19:31 +0530

    Create 3959-CheckGoodInteger

---
+++ 3959-CheckGoodInteger/3959-CheckGoodInteger.java
@@ -0,0 +1,14 @@
+// Last updated: 9/15/2026, 9:19:12 AM
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int digitsum = 0;
+        int squaresum = 0;
+        while(n>0){
+            int digit = n%10;
+            digitsum+=digit;
+            squaresum+=digit*digit;
+            n/=10;
+        }
+        return (squaresum-digitsum)>=50;
+    }
+}
