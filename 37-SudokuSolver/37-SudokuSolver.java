d98fdf692ca189503e92312b98f7638d528b70ee
Monikasri <monikasribn@gmail.com>
2026-09-15 09:15:53 +0530

    File updated at 9/15/2026, 9:15:34 AM

--- 37-SudokuSolver/37-SudokuSolver.java
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -1,42 +1,16 @@
-// Last updated: 9/15/2026, 9:13:01 AM
+// Last updated: 9/15/2026, 9:15:34 AM
 1class Solution {
-2    public void solveSudoku(char[][] board) {
-3        solve(board);
-4    }
-5
-6    private boolean solve(char[][] board) {
-7        for (int row = 0; row < 9; row++) {
-8            for (int col = 0; col < 9; col++) {
-9                if (board[row][col] == '.') {
-10                    for (char c = '1'; c <= '9'; c++) {
-11                        if (isValid(board, row, col, c)) {
-12                            board[row][col] = c;
-13
-14                            if (solve(board)) {
-15                                return true;
-16                            } else {
-17                                board[row][col] = '.'; 
-18                            }
-19                        }
-20                    }
-21                    return false; 
-22                }
-23            }
-24        }
-25        return true;
-26    }
-27
-28    private boolean isValid(char[][] board, int row, int col, char c) {
-29        for (int i = 0; i < 9; i++) {
-30          
-31            if (board[row][i] == c) return false;
-32         
-33            if (board[i][col] == c) return false;
-34         
-35            int subRow = 3 * (row / 3) + i / 3;
-36            int subCol = 3 * (col / 3) + i % 3;
-37            if (board[subRow][subCol] == c) return false;
-38        }
-39        return true;
-40    }
-41}
+2    public int countDigitOne(int n) {
+3        int count = 0;
+4
+5        for (long i = 1; i <= n; i *= 10) {
+6            long divider = i * 10;
+7            // Full complete cycles of 10^k
+8            count += (n / divider) * i;
+9            // Partial remainder cycle
+10            count += Math.min(Math.max(n % divider - i + 1, 0), i);
+11        }
+12
+13        return count;
+14    }
+15}
