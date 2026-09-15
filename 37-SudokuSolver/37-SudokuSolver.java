af02e0f45ab3b3406bceb31e212232dc4f1eb8a4
Monikasri <monikasribn@gmail.com>
2026-09-15 09:26:48 +0530

    File updated at 9/15/2026, 9:26:29 AM

--- 37-SudokuSolver/37-SudokuSolver.java
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -1,34 +1,46 @@
-// Last updated: 9/15/2026, 9:24:37 AM
-1import java.util.Collections;
-2import java.util.PriorityQueue;
-3
-4class MedianFinder {
-5    // Max-heap stores the smaller half of numbers
-6    private PriorityQueue<Integer> maxHeap;
-7    // Min-heap stores the larger half of numbers
-8    private PriorityQueue<Integer> minHeap;
-9
-10    public MedianFinder() {
-11        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-12        minHeap = new PriorityQueue<>();
-13    }
-14    
-15    public void addNum(int num) {
-16        maxHeap.offer(num);
-17        // Ensure all elements in maxHeap <= elements in minHeap
-18        minHeap.offer(maxHeap.poll());
-19        
-20        // Balance heap sizes so maxHeap has equal or 1 more element than minHeap
-21        if (maxHeap.size() < minHeap.size()) {
-22            maxHeap.offer(minHeap.poll());
-23        }
+// Last updated: 9/15/2026, 9:26:29 AM
+1import java.util.Arrays;
+2import java.util.LinkedList;
+3import java.util.Queue;
+4
+5public class Codec {
+6    private static final String NULL_NODE = "null";
+7    private static final String DELIMITER = ",";
+8
+9    // Encodes a tree to a single string.
+10    public String serialize(TreeNode root) {
+11        StringBuilder sb = new StringBuilder();
+12        serializeHelper(root, sb);
+13        return sb.toString();
+14    }
+15
+16    private void serializeHelper(TreeNode node, StringBuilder sb) {
+17        if (node == null) {
+18            sb.append(NULL_NODE).append(DELIMITER);
+19            return;
+20        }
+21        sb.append(node.val).append(DELIMITER);
+22        serializeHelper(node.left, sb);
+23        serializeHelper(node.right, sb);
 24    }
-25    
-26    public double findMedian() {
-27        if (maxHeap.size() > minHeap.size()) {
-28            return maxHeap.peek();
-29        } else {
-30            return (maxHeap.peek() + minHeap.peek()) / 2.0;
-31        }
-32    }
-33}
+25
+26    // Decodes your encoded data to tree.
+27    public TreeNode deserialize(String data) {
+28        Queue<String> nodes = new LinkedList<>(Arrays.asList(data.split(DELIMITER)));
+29        return deserializeHelper(nodes);
+30    }
+31
+32    private TreeNode deserializeHelper(Queue<String> nodes) {
+33        if (nodes.isEmpty()) return null;
+34        
+35        String val = nodes.poll();
+36        if (val.equals(NULL_NODE)) {
+37            return null;
+38        }
+39
+40        TreeNode node = new TreeNode(Integer.parseInt(val));
+41        node.left = deserializeHelper(nodes);
+42        node.right = deserializeHelper(nodes);
+43        return node;
+44    }
+45}
