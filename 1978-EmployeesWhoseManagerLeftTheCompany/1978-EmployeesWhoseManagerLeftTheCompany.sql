0a5fb2413cf029e8f7d40257316c0547868e3f2a
Monikasri <monikasribn@gmail.com>
2026-09-15 09:22:53 +0530

    Create 1978-EmployeesWhoseManagerLeftTheCompany

---
+++ 1978-EmployeesWhoseManagerLeftTheCompany/1978-EmployeesWhoseManagerLeftTheCompany.sql
@@ -0,0 +1,17 @@
+-- Last updated: 9/15/2026, 9:22:34 AM
+# Write your MySQL query statement below
+SELECT 
+    employee_id
+FROM 
+    Employees
+WHERE 
+    salary < 30000
+    AND manager_id IS NOT NULL
+    AND manager_id NOT IN (
+        SELECT 
+            employee_id 
+        FROM 
+            Employees
+    )
+ORDER BY 
+    employee_id ASC;
