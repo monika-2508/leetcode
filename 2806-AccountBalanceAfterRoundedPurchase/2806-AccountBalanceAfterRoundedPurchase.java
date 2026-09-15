a203cad351a1e00dd5f55438f56dc41b30caeef7
Monikasri <monikasribn@gmail.com>
2026-09-15 09:21:10 +0530

    Create 2806-AccountBalanceAfterRoundedPurchase

---
+++ 2806-AccountBalanceAfterRoundedPurchase/2806-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,8 @@
+// Last updated: 9/15/2026, 9:20:51 AM
+class Solution {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        int a = (purchaseAmount+5)/10;
+        return 100-( a*10);
+        
+    }
+}
