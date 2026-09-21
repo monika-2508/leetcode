08a4875578cb5c3b7d215f43f69ba8aba3d22490
Monikasri <monikasribn@gmail.com>
2026-09-21 13:36:01 +0530

    File updated at 9/21/2026, 1:35:42 PM

--- 13-RomantoInteger/13-RomantoInteger.java
+++ 13-RomantoInteger/13-RomantoInteger.java
@@ -1,28 +1,34 @@
-// Last updated: 9/21/2026, 1:33:05 PM
-1import java.util.ArrayList;
-2import java.util.Arrays;
-3import java.util.HashMap;
-4import java.util.List;
-5import java.util.Map;
-6
-7class Solution {
-8    public List<List<String>> groupAnagrams(String[] strs) {
-9        if (strs == null || strs.length == 0) {
-10            return new ArrayList<>();
-11        }
-12
-13        Map<String, List<String>> map = new HashMap<>();
-14
-15        for (String s : strs) {
-16            // Sort the characters of each string to use as the map key
-17            char[] chars = s.toCharArray();
-18            Arrays.sort(chars);
-19            String key = String.valueOf(chars);
-20
-21            map.putIfAbsent(key, new ArrayList<>());
-22            map.get(key).add(s);
+// Last updated: 9/21/2026, 1:35:42 PM
+1import java.util.ArrayDeque;
+2import java.util.Deque;
+3
+4class Solution {
+5    public String simplifyPath(String path) {
+6        Deque<String> stack = new ArrayDeque<>();
+7        String[] components = path.split("/");
+8
+9        for (String comp : components) {
+10            // Ignore empty strings (from multiple slashes like //) and current directory '.'
+11            if (comp.isEmpty() || comp.equals(".")) {
+12                continue;
+13            }
+14            if (comp.equals("..")) {
+15                // Move up one directory level if possible
+16                if (!stack.isEmpty()) {
+17                    stack.pollLast();
+18                }
+19            } else {
+20                // Push valid directory name onto the stack
+21                stack.offerLast(comp);
+22            }
 23        }
 24
-25        return new ArrayList<>(map.values());
-26    }
-27}
+25        // Build canonical path
+26        StringBuilder sb = new StringBuilder();
+27        for (String dir : stack) {
+28            sb.append("/").append(dir);
+29        }
+30
+31        return sb.length() == 0 ? "/" : sb.toString();
+32    }
+33}
