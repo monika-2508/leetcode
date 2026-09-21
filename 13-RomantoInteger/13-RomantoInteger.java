1cc24949bf1ea222fe0b6b7d11d11fed6d82391d
Monikasri <monikasribn@gmail.com>
2026-09-21 13:32:31 +0530

    File updated at 9/21/2026, 1:32:12 PM

--- 13-RomantoInteger/13-RomantoInteger.java
+++ 13-RomantoInteger/13-RomantoInteger.java
@@ -1,38 +1,30 @@
-// Last updated: 9/21/2026, 1:31:23 PM
+// Last updated: 9/21/2026, 1:32:12 PM
 1class Solution {
-2    public String multiply(String num1, String num2) {
-3        if ("0".equals(num1) || "0".equals(num2)) {
-4            return "0";
-5        }
-6
-7        int m = num1.length();
-8        int n = num2.length();
-9        int[] pos = new int[m + n];
-10
-11        // Multiply digits from right to left
-12        for (int i = m - 1; i >= 0; i--) {
-13            int d1 = num1.charAt(i) - '0';
-14            for (int j = n - 1; j >= 0; j--) {
-15                int d2 = num2.charAt(j) - '0';
-16                int mul = d1 * d2;
-17
-18                int p1 = i + j;
-19                int p2 = i + j + 1;
-20                int sum = mul + pos[p2];
-21
-22                pos[p2] = sum % 10;
-23                pos[p1] += sum / 10;
-24            }
+2    public String countAndSay(int n) {
+3        String current = "1";
+4        
+5        for (int i = 2; i <= n; i++) {
+6            StringBuilder next = new StringBuilder();
+7            int len = current.length();
+8            
+9            int j = 0;
+10            while (j < len) {
+11                char ch = current.charAt(j);
+12                int count = 0;
+13                
+14                // Count consecutive identical characters
+15                while (j < len && current.charAt(j) == ch) {
+16                    count++;
+17                    j++;
+18                }
+19                
+20                // Append frequency followed by the character
+21                next.append(count).append(ch);
+22            }
+23            
+24            current = next.toString();
 25        }
-26
-27        // Build string, omitting leading zeros
-28        StringBuilder sb = new StringBuilder();
-29        for (int p : pos) {
-30            if (!(sb.length() == 0 && p == 0)) {
-31                sb.append(p);
-32            }
-33        }
-34
-35        return sb.length() == 0 ? "0" : sb.toString();
-36    }
-37}
+26        
+27        return current;
+28    }
+29}
